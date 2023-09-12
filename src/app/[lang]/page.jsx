b9d85525b9844165f79513c157e6e1c485c8ee28
Lucian Felix Ghita <lucianfelix@gmail.com>
2023-09-12 20:58:12 +0200

    simple carousel

--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -1,15 +1,10 @@
-// import '#/../styles/globals.css';
-
 import {cache} from 'react';
 
 import Image from "next/image";
-import AdventureCard from "@/components/AdventureCard";
 import {AdventureClient} from "@/lib/adventures";
 import Carousel from "../../components/Carousel";
-
-
-const NEXT_PUBLIC_AEM_HOST = process.env.NEXT_PUBLIC_AEM_HOST;
-const NEXT_PUBLIC_AEM_ROOT = process.env.NEXT_PUBLIC_AEM_ROOT;
+import CarouselItem from "../../components/CarouselItem";
+import Link from "next/link";
 
 export const revalidate = 43200; // 12 hours in seconds
 export const dynamic = 'force-static';
@@ -20,10 +15,8 @@ export const preferredRegion = 'auto';
 const getFewAdventures = cache(async () => {
     const client = AdventureClient.fromEnv();
     const res = await client.getAllAdventures();
-    const adventures = res?.data?.adventureList?.items;
-    return adventures;
+    return res?.data?.adventureList?.items;
 });
-
 
 
 export async function generateStaticParams() {
@@ -34,21 +27,96 @@ export async function generateStaticParams() {
 
 export default async function Page({params: {lang}}) {
 
-    const adventures = await getFewAdventures(lang);
-
-    const images = [
-        'path/to/image1.jpg',
-        'path/to/image2.jpg',
-        'path/to/image3.jpg',
-        // ... add more image URLs as needed
-    ];
+    // const adventures = await getFewAdventures(lang);
+    //
+    // const images = [
+    //     'path/to/image1.jpg',
+    //     'path/to/image2.jpg',
+    //     'path/to/image3.jpg',
+    //     // ... add more image URLs as needed
+    // ];
 
     return (
-        <main className="bg-white py-10 mx-0 sm:py-16">
+        <main className="bg-white px-0 mx-0">
+            <div className="">
+                <Carousel className="h-[600px]">
+                    <CarouselItem>
+                        <div className="mx-0 lg:relative h-[600px]">
+                            <Image className="mx-0 w-full"
+                                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                   quality={75}
+                                   eager={true}
+                                   priority={true}
+                                   loading={'eager'}
+                                   alt={'hero'}
+                                   src="https://wknd.site/us/en/adventures/_jcr_content/root/container/teaser.coreimg.60.1600.jpeg/1660323801921/adobestock-216674449.jpeg"
+                                   width={1275}
+                                   height={717}
+                            />
+                            <div
+                                className="lg:absolute lg:bottom-0 max-w-[1136px] m-auto left-0 right-0 mh-5 lg:p-10 p-2 bg-white">
+                                <div className="text-xl">WKND Adventures</div>
+                                <div className="pt-5 text-base">Join us on one of our next adventures. Browse our list
+                                    of curated experiences and sign up for one when you&apos;re ready to explore with
+                                    us.
+                                </div>
+                                <div className="pt-10 pb-0"><Link href="adventures" className="p-4 bg-yellow uppercase">View
+                                    Trips</Link></div>
+                            </div>
 
-
-            <div className="container mx-auto">
-                <Carousel images={images} />
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                        <div className="mx-0 lg:relative h-[600px]">
+                            <Image className="mx-0 w-full"
+                                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                   quality={75}
+                                   eager={false}
+                                   priority={false}
+                                   alt={'hero'}
+                                   src="https://wknd.site/us/en/_jcr_content/root/container/carousel/item_1572035298405.coreimg.60.1600.jpeg/1660323822923/beach-walking.jpeg"
+                                   width={1275}
+                                   height={717}
+                            />
+                            <div
+                                className="lg:absolute lg:bottom-0 max-w-[1136px] m-auto left-0 right-0 mh-5 lg:p-10 p-2 bg-white">
+                                <div className="text-xl">San Diego Surf Spots</div>
+                                <div className="pt-5 text-base">From the hippie beaches of Ocean Beach to the ritzy
+                                    shores of La Jolla and everywhere in between. Discover the San Diego surf scene.
+                                </div>
+                                <div className="pt-10 pb-0"><Link href="adventures" className="p-4 bg-yellow uppercase">Full
+                                    Article</Link></div>
+                            </div>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                        <div className="mx-0 lg:relative h-[600px]">
+                            <Image className="mx-0 w-full"
+                                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                   quality={75}
+                                   eager={false}
+                                   priority={false}
+                                   alt={'hero'}
+                                   src="https://wknd.site/us/en/adventures/downhill-skiing-wyoming/_jcr_content/root/container/carousel/image.coreimg.60.1600.jpeg/1660323784078/adobestock-185234795.jpeg"
+                                   width={1275}
+                                   height={717}
+                            />
+                            <div
+                                className="lg:absolute lg:bottom-0 max-w-[1136px] m-auto left-0 right-0 mh-5 lg:p-10 p-2 bg-white">
+                                <div className="text-xl">Downhill Skiing Wyoming</div>
+                                <div className="pt-5 text-base">Experience wild untamed, rolling, wide-open terrain of
+                                    Wyoming in the winter.
+                                </div>
+                                <div className="pt-10 pb-0"><Link
+                                    href="adventures/downhill-skiing-wyoming/downhill-skiing-wyoming"
+                                    className="p-4 bg-yellow uppercase">Full Article</Link></div>
+                            </div>
+                        </div>
+                    </CarouselItem>
+                </Carousel>
+            </div>
+            <div className="p-20">
+                The Australian West coast is a camper’s heaven. Endless miles of desert roads leading to secret beaches, vast canyons and crystal clear rivers, and the very few people you are likely to meet on your journey will be some of the most easy-going characters you’ll find anywhere in the world.
             </div>
         </main>)
 }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -13,36 +13,65 @@
  *
  */
 
-import Link from 'next/link'
-import Image from 'next/image';
+import React, {useState} from 'react';
 
-import React from 'react';
-
-const Carousel = ({ images }) => {
+const Carousel = ({children, className}) => {
+    const [current, setCurrent] = useState(0);
+    const size = children.length;
+    //h-[250px]
     return (
-        <div className="relative w-full overflow-hidden">
-            <div className="carousel w-full flex transition-transform duration-500 ease-in-out">
-                {images.map((image, index) => (
-                    <img
-                        key={index}
-                        src={image}
-                        alt={`carousel-item-${index}`}
-                        className="w-full"
-                    />
-                ))}
+        <div className={"relative overflow-clip " + className}>
+            <div
+                className="carousel m-0 p-0 snap-x snap-mandatory flex transition-transform duration-500 ease-in-out">
+                {children}
             </div>
-            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 space-x-3 flex">
-                {images.map((_, index) => (
+            <div className="carousel-dots absolute bottom-4 left-1/2 transform -translate-x-1/2 space-x-3 flex">
+                {children.map((_, index) => (
                     <button
                         key={index}
                         onClick={() => {
                             const carousel = document.querySelector('.carousel');
                             carousel.style.transform = `translateX(-${index * 100}%)`;
+                            setCurrent(index);
                         }}
                         className="w-3 h-3 bg-gray-400 rounded-full focus:outline-none focus:bg-gray-600"
                     ></button>
                 ))}
             </div>
+            <button
+                onClick={() => {
+                    if (current === 0) {
+                        setCurrent(size - 1);
+                    } else {
+                        setCurrent(current - 1);
+                    }
+                    const carousel = document.querySelector('.carousel');
+                    carousel.style.transform = `translateX(-${current * 100}%)`;
+
+                    const carouselDots = document.querySelector('.carousel-dots');
+                    // focus on the current dot
+                    carouselDots.children[current].focus();
+                }}
+                className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2 bg-gray-400 rounded-full focus:outline-none focus:bg-gray-600"
+            >back
+            </button>
+            <button
+                onClick={() => {
+                    if (current === size - 1) {
+                        setCurrent(0);
+                    } else {
+                        setCurrent(current + 1);
+                    }
+                    const carousel = document.querySelector('.carousel');
+                    carousel.style.transform = `translateX(-${current * 100}%)`;
+
+                    const carouselDots = document.querySelector('.carousel-dots');
+                    // focus on the current dot
+                    carouselDots.children[current].focus();
+                }}
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 -translate-x-1/2 bg-gray-400 rounded-full focus:outline-none focus:bg-gray-600"
+            >next
+            </button>
         </div>
     );
 };
