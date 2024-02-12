842206db656200d44e61d6889d7c1beeb3677af8
Ghita Lucian <ghita@adobe.com>
2024-02-12 18:27:19 +0100

    prefix resource name

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
         viewport: "width=device-width, initial-scale=1.0, viewport-fit=cover",
         themeColor: "#000002",
         'urn:adobe:aue:config:service': "https://palma-dev-public.ethos14-stage-va7.ethos.adobe.net/api/aem-sites/wknd/gw/ue",
-        'urn:adobe:aue:system:palma': "palmaDemo",
+        'urn:adobe:aue:system:palma': "palma",
     }
 }
 
