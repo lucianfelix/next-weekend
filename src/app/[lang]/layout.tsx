fa25492a461b98f9b2c9b8ef81fad9854f60863c
Ghita Lucian <ghita@adobe.com>
2024-02-12 12:48:20 +0100

    static metadata

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -6,18 +6,29 @@ import Navigation from "@/components/Navigation";
 const inter = Inter({subsets: ['latin']})
 
 // @ts-ignore
-export async function generateMetadata({params: any}) {
-    var metadata = {
-        title: 'Your Next Weekend',
-        description: 'Find the best places to visit this weekend',
-        other: {
-            viewport: "width=device-width, initial-scale=1.0, viewport-fit=cover",
-            themeColor: "#000002",
-            'urn:adobe:aue:config:service': "https://palma-dev-public.ethos14-stage-va7.ethos.adobe.net/api/aem-sites/wknd/gw/ue",
-            'urn:adobe:aue:system:palma': "palmaDemo",
-        }
+// export async function generateMetadata({params: any}) {
+//     const metadata = {
+//         title: 'Your Next Weekend',
+//         description: 'Find the best places to visit this weekend',
+//         other: {
+//             viewport: "width=device-width, initial-scale=1.0, viewport-fit=cover",
+//             themeColor: "#000002",
+//             'urn:adobe:aue:config:service': "https://palma-dev-public.ethos14-stage-va7.ethos.adobe.net/api/aem-sites/wknd/gw/ue",
+//             'urn:adobe:aue:system:palma': "palmaDemo",
+//         }
+//     };
+//     return metadata;
+// }
+
+export const metadata: Metadata = {
+    title: 'Your Next Weekend',
+    description: 'Find the best places to visit this weekend',
+    other: {
+        viewport: "width=device-width, initial-scale=1.0, viewport-fit=cover",
+        themeColor: "#000002",
+        'urn:adobe:aue:config:service': "https://palma-dev-public.ethos14-stage-va7.ethos.adobe.net/api/aem-sites/wknd/gw/ue",
+        'urn:adobe:aue:system:palma': "palmaDemo",
     }
-    return metadata;
 }
 
 export default function RootLayout({
