c6f99139b492e2f577d420535316757fc1d716bc
Ghita Lucian <ghita@adobe.com>
2024-05-20 12:27:49 +0200

    Page api changes

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
+import {ueApiUrl} from "@/lib/headless_openai";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -28,7 +29,7 @@ export const metadata: Metadata = {
     other: {
         viewport: "width=device-width, initial-scale=1.0, viewport-fit=cover",
         themeColor: "#000002",
-        'urn:adobe:aue:config:service': "https://palma-dev-public.ethos14-stage-va7.ethos.adobe.net/api/aem-sites/wknd/gw/ue",
+        'urn:adobe:aue:config:service': ueApiUrl,
         'urn:adobe:aue:system:palma': "https://author-p92452-e211791-cmstg.adobeaemcloud.com",
     }
 }
